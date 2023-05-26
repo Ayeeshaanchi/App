33d697e53ebf3fdd1899c0607ee11f03049437e5
sonukuldeep <sonukuldeep@outlook.com>
2023-05-26 21:28:41 +0530

    about section and testimonials done. Responsiveness remain

--- blog_data.js
+++ blog_data.js
@@ -16,6 +16,23 @@ const blog_data = {
             "para": "Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae"
         }
 
+    ],
+    testimonials: [
+        {
+            "image": "assets/images/5f2ca4fff547c01771bceb8b_avatar_1.jpg",
+            "title": "Miriam Davis",
+            "body": "Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur"
+        },
+        {
+            "image": "assets/images/5f2ca4ffe88d879f9b7a68ac_avatar_2.jpg",
+            "title": "Ellen Evans",
+            "body": "Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam"
+        },
+        {
+            "image": "assets/images/5f2ca500bd2498f8320739a2_avatar_3.jpg",
+            "title": "Christine Dean",
+            "body": "Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus"
+        }
     ]
 }
 
